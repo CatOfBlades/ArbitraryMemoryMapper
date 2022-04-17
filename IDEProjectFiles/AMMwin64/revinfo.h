28c89206a2aab5ea41967f1e8503ba6a0644bfce
CatOfBlades <Fergley@yahoo.com>
2022-04-16 21:42:12 -0400

    New BuildScript dir
    Setting up for better control over different OS build scripts.

---
+++ IDEProjectFiles/AMMwin64/revinfo.h
@@ -0,0 +1,3 @@
+// file automatically generated on build, do not edit.
+#include <string>
+std::string BuildInfo = "commit 11db09c7a402c66e7ddbde5c1c2a2d7e8e6a3a93 Author: CatOfBlades <Fergley@yahoo.com> Date:   Sat Apr 16 19:01:37 2022 -0400build at: 2022-04-16-20.16.57 ";
--- revinfo.h
+++
@@ -1,3 +0,0 @@
-// file automatically generated on build, do not edit.
-#include <string>
-std::string BuildInfo = "commit 0c61cb86e623772bde46fb56651a6effca74c40d Author: CatOfBlades <Fergley@yahoo.com> Date:   Sat Apr 16 18:18:10 2022 -0400build at: 2022-04-16-18.57.32 ";
