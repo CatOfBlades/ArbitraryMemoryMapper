9f33595622e772e164c82de7b8d1fbec054ef7eb
CatOfBlades <Fergley@yahoo.com>
2024-04-11 03:18:52 -0400

    Update MemoryMap.lua

--- LuaScripts/MemoryMap.lua
+++ LuaScripts/MemoryMap.lua
@@ -21,14 +21,16 @@ print("test memory functions")
 
 createMemoryContext("mem1")
 addVirtualPage("page1",256)
-addVirtualPage("page2",256)
-addMultiPage("page3",2,{"page1","page2"})
---linkPageToMemorySpace("mem1","page1")
+--addVirtualPage("page2",256)
+--addMultiPage("page3",2,{"page1","page2"})
+linkPageToMemorySpace("mem1","page1")
 --linkPageToMemorySpace("mem1","page2")
-linkPageToMemorySpace("mem1","page3") --linking the multipage as the first page to show off memory swapping potential
+--linkPageToMemorySpace("mem1","page3") --linking the multipage as the first page to show off memory swapping potential
 
 print("memory space initialized for testing")
 SysBeep(600,200)
+
+print("preforming data write test")
 length = 10
 i = 1
 j = {}
@@ -37,17 +39,17 @@ while i<=length do
 	j[i] = i
 	i=i+1
 end
-multiPageSwapBanks("page3", 0) --bank numbers start at 0 so it is the first bank and 1 is the seccond bank
+--multiPageSwapBanks("page3", 0) --bank numbers start at 0 so it is the first bank and 1 is the seccond bank
 writeMemToContext("mem1",0,length,j)
-multiPageSwapBanks("page3", 1)
+--multiPageSwapBanks("page3", 1)
 while i>0 do
 	j[length-i+1] = i
 	i=i-1
 end
-writeMemToContext("mem1",0,length,j)
-print("multibank pages ready")
+--writeMemToContext("mem1",0,length,j)
+print("data writen ready for read test")
 
-multiPageSwapBanks("page3", 0) --swapping back to bank 0 (page1)
+--multiPageSwapBanks("page3", 0) --swapping back to bank 0 (page1)
 k = readMemFromContext("mem1",0,length) --note that we are reading from address '0' this is the "top" of our memory space.
 
 --printing the contents of bank 0
@@ -57,16 +59,16 @@ while i<=length do
 	i=i+1
 end
 
-multiPageSwapBanks("page3", 1) --swapping to bank 1 (page2)
-k = readMemFromContext("mem1",0,length)
+--multiPageSwapBanks("page3", 1) --swapping to bank 1 (page2)
+--k = readMemFromContext("mem1",0,length)
 
 --printing the contents of bank 1 to show they are different.
-i=0
-while i<=length do
-	print(k[i])
-	i=i+1
-end
+--i=0
+--while i<=length do
+--	print(k[i])
+--	i=i+1
+--end
 
 destroyPage("page1") --we have to clean these up manually because multipages don't destroy pages they refrence.
-destroyPage("page2") --note: destroying a page frees it from the memoryContext that its linked to. So you can replace it by linking another.
+--destroyPage("page2") --note: destroying a page frees it from the memoryContext that its linked to. So you can replace it by linking another.
 destroyMemoryContext("mem1") --page3 is destroyed by the memoryContext when it dies because they are linked.
